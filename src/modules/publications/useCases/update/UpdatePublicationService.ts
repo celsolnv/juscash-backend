05220949d7c9c699a58605099d5cefd2625a51ef
Celso Leandro <celsolnv27@gmail.com>
2025-06-23 17:03:05 -0300

    FEAT: add rules for update

--- src/modules/publications/useCases/update/UpdatePublicationService.ts
+++ src/modules/publications/useCases/update/UpdatePublicationService.ts
@@ -2,6 +2,7 @@ import { ValidateDataHelperMethod } from '../../../../utils/helpers/ValidateData
 import { Resolve } from '../../../../libs/return/index';
 import { IPublicationRepository } from '../../repositories/IPublicationRepository';
 import { IUpdatePublicationDto } from '../../dtos/IUpdatePublicationDto';
+import { PublicationStatus } from '../../../../entities/Publication';
 
 
 export class UpdatePublicationService {
@@ -15,8 +16,55 @@ export class UpdatePublicationService {
   ): Promise<Resolve> {
     await this.validateData(data);
 
+    const publicationExist = await this.repository.findById(id);
+    if (!publicationExist) {
+      return {
+        status: 404, data: { message: "Publicação não encontrada!" },
+        success: false
+      };
+    }
+    if (publicationExist.status === "done") {
+      return {
+        status: 400, data: { message: "Publicação já finalizada!" },
+        success: false
+      };
+    }
+    if (!data.status) {
+      return {
+        status: 400, data: { message: "Status é obrigatório!" },
+        success: false
+      };
+    }
+
+    const checkValidMove = this.checkValidMove(
+      publicationExist.status,
+      data.status,
+    );
+    if (!checkValidMove) {
+      return {
+        status: 400,
+        data: { message: "Movimentação inválida! Entre em contato com o administrador" },
+        success: false,
+      };
+    }
     const publication = await this.repository.update(id, data);
     return { status: 200, data: publication, success: true };
+  }
+
+  public checkValidMove(
+    currentStatus: PublicationStatus,
+    newStatus: PublicationStatus
+  ): boolean {
+    if (currentStatus === "new" && newStatus === "read") {
+      return true;
+    } else if (currentStatus === "read" && newStatus === "sent_to_lawyer") {
+      return true;
+    } else if (currentStatus === "sent_to_lawyer" && newStatus === "read") {
+      return true;
+    } else if (currentStatus === "sent_to_lawyer" && newStatus === "done") {
+      return true;
+    }
+    return false;
   }
 
   public async validateData(data: IUpdatePublicationDto) {
